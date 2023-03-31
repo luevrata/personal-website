8e3ac383cf959949836fa2a6a2c608a235157287
elizaveta.firsova <elizaveta.firsova@workday.com>
2023-03-30 20:04:07 -0700

    commented activities section

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,32 @@ import Experience from "./components/experience/Experience";
 import Projects from "./components/projects/Projects";
 import Contacts from "./components/contacts/Contacts";
 import Footer from "./components/footer/Footer";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Activities from "./components/activities/Activities";
 
-const App =() => {
-
-    return(
+const App = () => {
+  return (
     <div>
-        <Helmet>
-            <meta charSet="utf-8" />
-            <title>Elizaveta Firsova</title>
-            <link rel="canonical" href="http://mysite.com/example" />
-            <meta name="description" content="Elizaveta Firsova Website" />
-        </Helmet>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Elizaveta Firsova</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Elizaveta Firsova Website" />
+      </Helmet>
 
-        <Header/>
-        <Nav/>
-        <About/>
-        <Experience/>
-        <Projects/>
-        <Activities/>
-        <Contacts/>
-        <Footer/>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Projects />
+      {/* <Activities/> */}
+      <Contacts />
+      <Footer />
     </div>
-    )
+  );
+};
 
-}
-
-export default App
+export default App;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
-import './nav.css'
-import {HiUserCircle, HiHome} from "react-icons/hi"
-import {MdHomeRepairService} from "react-icons/md"
-import {RiMessage2Fill} from "react-icons/ri"
-import {GiHeartInside} from 'react-icons/gi'
-import {FaBook} from 'react-icons/fa'
-import { useState } from 'react'
+import React from "react";
+import "./nav.css";
+import { HiUserCircle, HiHome } from "react-icons/hi";
+import { MdHomeRepairService } from "react-icons/md";
+import { RiMessage2Fill } from "react-icons/ri";
+import { GiHeartInside } from "react-icons/gi";
+import { FaBook } from "react-icons/fa";
+import { useState } from "react";
 
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState('#')
-  const areaReferences = ['#', '#about', '#experience', '#projects', '#activities', '#contacts']
-  const icons = [<HiHome/>, <HiUserCircle/>, <MdHomeRepairService/>, <FaBook/>, <GiHeartInside/>, <RiMessage2Fill/>]
+  const [activeNav, setActiveNav] = useState("#");
+  const areaReferences = [
+    "#",
+    "#about",
+    "#experience",
+    "#projects",
+    // "#activities",
+    "#contacts",
+  ];
+  const icons = [
+    <HiHome />,
+    <HiUserCircle />,
+    <MdHomeRepairService />,
+    <FaBook />,
+    // <GiHeartInside />,
+    <RiMessage2Fill />,
+  ];
   return (
     <nav>
-      {areaReferences.map((reference, index)=><a href={reference} onClick={()=>setActiveNav(reference)} className={activeNav === reference?'active':''}> {icons[index]}</a>)}
+      {areaReferences.map((reference, index) => (
+        <a
+          href={reference}
+          onClick={() => setActiveNav(reference)}
+          className={activeNav === reference ? "active" : ""}
+        >
+          {" "}
+          {icons[index]}
+        </a>
+      ))}
     </nav>
-  )
-}
-export default Nav
+  );
+};
+export default Nav;
